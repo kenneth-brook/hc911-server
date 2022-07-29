1e732564698216b136b6b29a12939f20a0891397
Kenneth Brook <kenneth.brook@hotmail.com>
2022-07-29 16:48:22 -0400

    header middleware added

--- api.js
+++ api.js
@@ -16,6 +16,12 @@ router.use((request, response, next) => {
     next();
   });
 
+  app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
+
 
   router.route('/calls').get((request, response) => {
     Db.getCalls().then((data) => {
