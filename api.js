d15490be70c9ea73eb09116fde2173505b82a8ae
Kenneth Brook <kenneth.brook@hotmail.com>
2022-07-29 11:05:40 -0400

    port adjustment 2

--- api.js
+++ api.js
@@ -25,6 +25,6 @@ router.use((request, response, next) => {
 
 
 
-let port = process.env.PORT || 443;
+let port = process.env.PORT || 3000;
 app.listen(port);
 console.log('call API is runnning at ' + port);
