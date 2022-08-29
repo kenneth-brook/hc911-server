aae1d0d62a7bf299daf93b48769d6d5f3e414f01
Kenneth Brook <kenneth.brook@hotmail.com>
2022-08-29 09:36:27 -0400

    roll back

--- api.js
+++ api.js
@@ -7,18 +7,6 @@ const  cors = require('cors');
 const  app = express();
 const  router = express.Router();
 const  sql = require('mssql');
-const fs = require('fs');
-const https = require('https');
-
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/hc911server.365dtm.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/hc911server.365dtm.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/hc911server.365dtm.com/chain.pem', 'utf8');
-
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
 
 
 app.use(bodyParser.urlencoded({ extended:  true }));
@@ -45,11 +33,11 @@ router.use((request, response, next) => {
     next();
   });
 
-  //app.all('/', function(req, res, next) {
-    //res.header("Access-Control-Allow-Origin", "*");
-    //res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    //next()
-  //});
+  app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 
   router.route('/calls').get((request, response) => {
     
@@ -64,11 +52,8 @@ router.use((request, response, next) => {
       response.json(data[0]);
     })
   });
-  
-  const httpsServer = https.createServer(credentials, app);
+
 let port = 8675;
-httpsServer.listen(port, () => {
-	console.log('HTTPS Server running on port 443');
-});
+app.listen(port);
 console.log('call API is runnning at ' + port);
 
